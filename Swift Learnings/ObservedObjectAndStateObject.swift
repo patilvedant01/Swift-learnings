5b041fb6425cbc6a9994b019ab7d68943cb7ec54
patilvedant01 <vedantpatil324@gmail.com>
2024-09-11 14:22:37 +0530

    firstCommit

---
+++ Swift Learnings/ObservedObjectAndStateObject.swift
@@ -0,0 +1,18 @@
+//
+//  ObservedObjectAndStateObject.swift
+//  Swift Learnings
+//
+//  Created by Vedant Patil on 11/09/24.
+//
+
+import SwiftUI
+
+struct ObservedObjectAndStateObject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservedObjectAndStateObject()
+}
